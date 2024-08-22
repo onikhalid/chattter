77a51811690ee1c08072c4fa1f4968a624a2eff4
Oni Khalid Ayòbámi <onikhalidayo@gmail.com>
2024-08-22 17:07:49 +0100

    fix comments drawer

--- src/app/(main)/(authenticated-user)/p/[post_id]/@modal/(.)discuss/page.tsx
+++ src/app/(main)/(authenticated-user)/p/[post_id]/@modal/(.)discuss/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { CommentsDrawer } from '@/app/(main)/(authenticated-user)/misc/components'
 
-const InterceptedDiscussPage = ({ searchParams: { post_id } }: { searchParams: { post_id: string } }) => {
+const InterceptedDiscussPage = ({ params: { post_id } }: { params: { post_id: string } }) => {
   return (
     <CommentsDrawer searchParams={{ post_id }} />
   )
