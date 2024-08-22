a3d414ffea9e4d0f9f02d373e42d672444526ff5
Oni Khalid Ayòbámi <onikhalidayo@gmail.com>
2024-08-22 13:46:54 +0100

    Update page.tsx

--- src/app/(main)/(authenticated-user)/new/page.tsx
+++ src/app/(main)/(authenticated-user)/new/page.tsx
@@ -9,6 +9,7 @@ import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import 'react-quill/dist/quill.snow.css';
 import { z, ZodError } from "zod";
+import {SaveIcon, SendIcon } from 'lucide-react'
 
 import { Button, FormError, Input, LoadingModal, TagInput, Textarea } from '@/components/ui'
 import { auth, storage } from '@/utils/firebaseConfig';
@@ -113,7 +114,7 @@ const WriteNewStoryPage = () => {
 
     const handleCreateNewPost = async (data: createNewPostFormDataType) => {
         const submittedData = data;
-        const currentContent = watch('content');
+        const currentContent = editorMode === 'richText' ? watch('content') : convertHtmlToMarkdown(watch('content'));
         const currentImages = extractImageUrls(currentContent);
 
         const deletedImages = uploadedImages.filter(
@@ -426,6 +427,17 @@ const WriteNewStoryPage = () => {
                         </div>
                     )}
                 />
+                <Button shape='rounded' variant="secondary" className='flex items-center gap-2 rounded-lg py-1.5' type='submit' form="form">
+                    <span className=''>
+                      {postToEditId ? "Update" : "Submit"}
+                    </span>
+                    {
+                      postToEditId ?
+                        <SaveIcon size={15} />
+                        :
+                        <SendIcon size={15} />
+                    }
+                  </Button>
             </form>
 
 
